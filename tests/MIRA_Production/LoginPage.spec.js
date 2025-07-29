dffda6e764f75bad8db64c53906652658bbbadf1
HoJade <chunho0818@gmail.com>
2025-07-29 11:33:45 -0400

    add test for non-1st time visit, refine try {} condition

--- tests/MIRA_Production/LoginPage.spec.js
+++ tests/MIRA_Production/LoginPage.spec.js
@@ -63,107 +63,145 @@ test.beforeEach('Land to MIRA Production portal login page', async ({ page }) =>
 
 test.describe('MIRA Production portal Login page', () => {
 
-    test('check elements on MIRA Production portal Login page', async ({ page, headless }) => {
-        // check header
-        await expect(page.locator('[header="Sign In"]')).toBeVisible()
-        // check Username field
-        const username = page.locator('[inputid="username"]')
-        await expect(username).toBeVisible();
-        await expect(username).toHaveText(/Username/);
-        await expect(username.getByLabel("Username")).toBeVisible()
-        // check Password field
-        const password = page.locator('[inputid="password"]')
-        await expect(password).toBeVisible();
-        await expect(password).toHaveText(/Password/);
-        await expect(password.getByLabel("Password")).toBeVisible()
-        // check button
-        await expect(page.getByRole('button', { name: 'Login' })).toBeVisible()
+    test.describe('1st time visit', () => {
 
-        // do image comparison WHEN run in headed mode
-        if (headless) {
-            await expect.soft(page).toHaveScreenshot()
-        } else {
-            console.log('Skipping screenshot comparison for headed mode.')
-        }
-    })
+        test('check elements on MIRA Production portal Login page', async ({ page, headless }) => {
+            // check header
+            await expect(page.locator('[header="Sign In"]')).toBeVisible()
+            // check Username field
+            const username = page.locator('[inputid="username"]')
+            await expect(username).toBeVisible();
+            await expect(username).toHaveText(/Username/);
+            await expect(username.getByLabel("Username")).toBeVisible()
+            // check Password field
+            const password = page.locator('[inputid="password"]')
+            await expect(password).toBeVisible();
+            await expect(password).toHaveText(/Password/);
+            await expect(password.getByLabel("Password")).toBeVisible()
+            // check button
+            await expect(page.getByRole('button', { name: 'Login' })).toBeVisible()
+
+            // do image comparison WHEN run in headed mode
+            if (headless) {
+                await expect.soft(page).toHaveScreenshot()
+            } else {
+                console.log('Skipping screenshot comparison for headed mode.')
+            }
+        });
 
 
-    test('login_success', async ({ page, headless }) => {
-        // Username
-        await page.getByLabel('Username').click();
-        await page.getByLabel('Username').fill(credentials.customerName_true);
-        console.log('Username:', credentials.customerName_true)
-        // Password
-        await page.getByLabel('Password').click();
-        await page.getByLabel('Password').fill(credentials.customerPassword_true);
-        console.log('Password:', credentials.customerPassword_true)
-        // Log-in
-        await page.getByRole('button', { name: 'Login' }).click();
+        test('login_success', async ({ page, headless }) => {
+            // Username
+            await page.getByLabel('Username').click();
+            await page.getByLabel('Username').fill(credentials.customerName_true);
+            console.log('Username:', credentials.customerName_true)
+            // Password
+            await page.getByLabel('Password').click();
+            await page.getByLabel('Password').fill(credentials.customerPassword_true);
+            console.log('Password:', credentials.customerPassword_true)
 
-        // land to MIRA Production portal main landing page
-        try {
-            await page.waitForURL(/products/)
-        } catch (error) {
-            console.log('Login process took too long:', error)
-        }
-        await expect(page).toHaveURL(/products/);
-        const login_success_tooltip = page.getByRole('alert')
-        await login_success_tooltip.hover()
-        await expect(login_success_tooltip).toBeVisible()
-        await expect(login_success_tooltip.locator('[data-pc-section="detail"]')).toHaveText(credentials.login_success_message)
-        // do image comparison WHEN run in headed mode
-        if (headless) {
-            await expect.soft(login_success_tooltip).toHaveScreenshot()
-        } else {
-            console.log('Skipping screenshot comparison for headed mode.')
-        }
+            // Log-in
+            try {
+                await Promise.all([
+                    // wait to land to MIRA Production portal
+                    page.waitForURL(/products/),
+                    // click on the Login button
+                    page.getByRole('button', { name: 'Login' }).click(),
+                ])
+            } catch (error) {
+                console.log('Login process took too long:', error)
+            }
+            // land to MIRA Production portal main landing page
+            await expect(page).toHaveURL(/products/);
+            const login_success_tooltip = page.getByRole('alert')
+            await login_success_tooltip.hover()
+            await expect(login_success_tooltip).toBeVisible()
+            await expect(login_success_tooltip.locator('[data-pc-section="detail"]')).toHaveText(credentials.login_success_message)
+            // do image comparison WHEN run in headed mode
+            if (headless) {
+                await expect.soft(login_success_tooltip).toHaveScreenshot()
+            } else {
+                console.log('Skipping screenshot comparison for headed mode.')
+            }
+        });
+
+
+        test('login_fail_username', async ({ page, headless }) => {
+            // Username
+            await page.getByLabel('Username').fill(credentials.username_false);
+            console.log('Username:', credentials.username_false)
+            // Password
+            await page.getByLabel('Password').fill(credentials.password_false);
+            console.log('Password:', credentials.password_false)
+            // Log-in
+            await page.getByRole('button', { name: 'Login' }).click();
+
+            // check error message
+            const login_fail_tooltip = page.getByRole('alert')
+            await login_fail_tooltip.hover()
+            await expect(login_fail_tooltip).toBeVisible()
+            await expect(login_fail_tooltip.locator('[data-pc-section="detail"]')).toHaveText(credentials.login_username_fail_message)
+            // do image comparison WHEN run in headed mode
+            if (headless) {
+                await expect.soft(login_fail_tooltip).toHaveScreenshot()
+            } else {
+                console.log('Skipping screenshot comparison for headed mode.')
+            }
+        });
+
+
+        test('login_fail_password', async ({ page, headless }) => {
+            // Username
+            await page.getByLabel('Username').fill(credentials.customerName_true);
+            console.log('Username:', credentials.customerName_true)
+            // Password
+            await page.getByLabel('Password').fill(credentials.password_false);
+            console.log('Password:', credentials.password_false)
+            // Log-in
+            await page.getByRole('button', { name: 'Login' }).click();
+
+            // check error message
+            const login_fail_tooltip = page.getByRole('alert')
+            await login_fail_tooltip.hover()
+            await expect(login_fail_tooltip).toBeVisible()
+            await expect(login_fail_tooltip.locator('[data-pc-section="detail"]')).toHaveText(credentials.login_password_fail_message)
+            // do image comparison WHEN run in headed mode
+            if (headless) {
+                await expect.soft(login_fail_tooltip).toHaveScreenshot()
+            } else {
+                console.log('Skipping screenshot comparison for headed mode.')
+            }
+        });
     });
 
 
-    test('login_fail_username', async ({ page, headless }) => {
-        // Username
-        await page.getByLabel('Username').fill(credentials.username_false);
-        console.log('Username:', credentials.username_false)
-        // Password
-        await page.getByLabel('Password').fill(credentials.password_false);
-        console.log('Password:', credentials.password_false)
-        // Log-in
-        await page.getByRole('button', { name: 'Login' }).click();
 
-        // check error message
-        const login_fail_tooltip = page.getByRole('alert')
-        await login_fail_tooltip.hover()
-        await expect(login_fail_tooltip).toBeVisible()
-        await expect(login_fail_tooltip.locator('[data-pc-section="detail"]')).toHaveText(credentials.login_username_fail_message)
-        // do image comparison WHEN run in headed mode
-        if (headless) {
-            await expect.soft(login_fail_tooltip).toHaveScreenshot()
-        } else {
-            console.log('Skipping screenshot comparison for headed mode.')
-        }
-    });
+    test.describe('non-1st time visit', () => {
+
+        test.beforeEach('Login to MIRA Admin portal', async ({ page }) => {
+            // land to MIRA Production portal login page
+            await page.goto(`${credentials.mira_production_baseURL}/login`);
+            await expect(page.locator('app-login')).toBeVisible();
+            await page.getByLabel('Username').fill(credentials.customerName_true);
+            await page.getByLabel('Password').fill(credentials.customerPassword_true);
+            await Promise.all([
+                // wait to land to MIRA Production portal
+                page.waitForSelector('p-menu', { state: 'visible', timeout: 20000 }),
+                // click on the Login button
+                page.getByRole('button', { name: 'Login' }).click(),
+            ])
+        });
+
+        test.beforeEach('Sign out from MIRA Production portal', async ({ page }) => {
+            // sign out from MIRA Production portal
+            await page.getByRole('button', { name: 'Sign out' }).click();
+            // wait to land to MIRA Production portal login page
+            await expect(page).toHaveURL(/\/login\/?$/)
+        });
 
 
-    test('login_fail_password', async ({ page, headless }) => {
-        // Username
-        await page.getByLabel('Username').fill(credentials.customerName_true);
-        console.log('Username:', credentials.customerName_true)
-        // Password
-        await page.getByLabel('Password').fill(credentials.password_false);
-        console.log('Password:', credentials.password_false)
-        // Log-in
-        await page.getByRole('button', { name: 'Login' }).click();
-
-        // check error message
-        const login_fail_tooltip = page.getByRole('alert')
-        await login_fail_tooltip.hover()
-        await expect(login_fail_tooltip).toBeVisible()
-        await expect(login_fail_tooltip.locator('[data-pc-section="detail"]')).toHaveText(credentials.login_password_fail_message)
-        // do image comparison WHEN run in headed mode
-        if (headless) {
-            await expect.soft(login_fail_tooltip).toHaveScreenshot()
-        } else {
-            console.log('Skipping screenshot comparison for headed mode.')
-        }
+        test('login page cached Customer Username value', async ({ page, headless }) => {
+            await expect(page.getByRole('button', { name: 'New Customer' })).toBeVisible();
+        });
     });
 });
